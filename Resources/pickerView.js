c47844a0b624d9c99e800eb21bc1510a4fa39898
araki-k <araki-k@dhcp-10-11-70-038.35a.klab.org>
2014-01-30 21:36:03 +0900

    searchWindow.jsにareaPickerViewを追加

--- Resources/pickerView.js
+++ Resources/pickerView.js
@@ -1,68 +1,35 @@
+function pickerView() {
 
-Ti.API.info("++++++++++++++++++++++");
+	var self = Titanium.UI.createView({
+		height: 251,
+		bottom: -251
+	});
+	
+	var doneButton = Titanium.UI.createButton({
+		title: '完了',
+		style: Titanium.UI.iPhone.SystemButtonStyle.DONE
+	});
 
-var picker_view = Titanium.UI.createView({
-	height: 251,
-	bottom: -251
-});
+	var spacer = Titanium.UI.createButton({
+		systemButton: Titanium.UI.iPhone.SystemButton.FLEXIBLE_SPACE
+	});
 
-var cancelButton = Titanium.UI.createButton({
-	title: 'Cancel',
-	style: Titanium.UI.iPhone.SystemButtonStyle.BORDERED
-});
+	var toolbar = Titanium.UI.iOS.createToolbar({
+		top: 0,
+		items: [spacer, doneButton]
+	});
 
-var doneButton = Titanium.UI.createButton({
-	title: 'Done',
-	style: Titanium.UI.iPhone.SystemButtonStyle.DONE
-});
+	var picker = Ti.UI.createPicker({
+		top: 43
+	});
+	picker.selectionIndicator = true;
 
-var spacer = Titanium.UI.createButton({
-	systemButton: Titanium.UI.iPhone.SystemButton.FLEXIBLE_SPACE
-});
+	self.add(toolbar);
+	self.add(picker);
+	
+	return self;
 
-var toolbar = Titanium.UI.iOS.createToolbar({
-	top: 0,
-	items: [cancelButton, spacer, doneButton]
-});
+}
 
-var picker = Ti.UI.createPicker({
-	top: 43
-});
-picker.selectionIndicator = true;
+module.exports = pickerView;
 
-var data = [];
-data[0]=Ti.UI.createPickerRow({title:'18〜19歳',custom_item:'1'});
-data[1]=Ti.UI.createPickerRow({title:'20代前半',custom_item:'2'});
-data[2]=Ti.UI.createPickerRow({title:'20代半ば',custom_item:'3'});
-data[3]=Ti.UI.createPickerRow({title:'20代後半',custom_item:'4'});
-data[4]=Ti.UI.createPickerRow({title:'30代前半',custom_item:'5'});
-data[5]=Ti.UI.createPickerRow({title:'30代半ば',custom_item:'6'});
-data[6]=Ti.UI.createPickerRow({title:'30代後半',custom_item:'7'});
-data[7]=Ti.UI.createPickerRow({title:'40代前半',custom_item:'8'});
-data[8]=Ti.UI.createPickerRow({title:'40代半ば',custom_item:'9'});
-data[9]=Ti.UI.createPickerRow({title:'40代後半',custom_item:'10'});
-data[10]=Ti.UI.createPickerRow({title:'50代以上',custom_item:'11'});
-
-picker.add(data);
-picker_view.add(toolbar);
-picker_view.add(picker);
-
-//=======================================================================================
-//外部公開ファンクション
-//=======================================================================================
-
-exports.pickerView = function() {
-	return picker_view;
-};
-
-exports.picker = function() {
-	return picker;
-};
-
-exports.cancelButton = function() {
-	return cancelButton;
-};
-
-exports.doneButton = function() {
-	return doneButton;
-};
--- Resources/searchWindow.js
+++ Resources/searchWindow.js
@@ -1,28 +1,14 @@
 function searchWindow() {
 
-	var searchWindow;
-	var textField;
-	var pickerView;
-	var picker;
-	var cancelButton;
-	var doneButton;
-	
-	var slideIn = Titanium.UI.createAnimation({
-		bottom: 0,
-		duration: 300
-	});
-	
-	var slideOut = Titanium.UI.createAnimation({
-		bottom: -251,
-		duration: 300
-	});
-
 	var self = Titanium.UI.createWindow({  
     	title:'探す',
     	backgroundColor:'#fff'
 	});
 	
-	var textField = Titanium.UI.createTextField({
+	//=========================================
+	//age系要素の定義
+	//=========================================
+	var ageTextField = Titanium.UI.createTextField({
 		borderStyle: Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
 		top: 20,
 		right: 50,
@@ -30,48 +16,116 @@ function searchWindow() {
 		enabled: false,
 		keyboardToolbar: false
 	});
-		
-	self.add(textField);
 	
-	var pView = require('pickerView').pickerView;
-	pickerView = new pView();
+	//ageピッカービューのインスタンス生成
+	var agePView = require('pickerView');
+	var agePickerView = new agePView();
 	
-	var p = require('pickerView').picker;
-	picker = new p();
+	var ageChildren = agePickerView.getChildren();
+	var ageToolbar = ageChildren[0];
+	var agePicker = ageChildren[1];
 	
-	var cButton = require('pickerView').cancelButton;
-	cancelButton = new cButton();
+	var ageItems = ageToolbar.getItems();
+	var ageDoneButton = ageItems[1];
 	
-	var dButton = require('pickerView').doneButton;
-	doneButton = new dButton();
+	//ピッカーにageデータを挿入
+	var ageData = [];
+	ageData[0] = Ti.UI.createPickerRow({title:'18〜19歳',custom_item:'1'});
+	ageData[1] = Ti.UI.createPickerRow({title:'20代前半',custom_item:'2'});
+	ageData[2] = Ti.UI.createPickerRow({title:'20代半ば',custom_item:'3'});
+	ageData[3] = Ti.UI.createPickerRow({title:'20代後半',custom_item:'4'});
+	ageData[4] = Ti.UI.createPickerRow({title:'30代前半',custom_item:'5'});
+	ageData[5] = Ti.UI.createPickerRow({title:'30代半ば',custom_item:'6'});
+	ageData[6] = Ti.UI.createPickerRow({title:'30代後半',custom_item:'7'});
+	ageData[7] = Ti.UI.createPickerRow({title:'40代前半',custom_item:'8'});
+	ageData[8] = Ti.UI.createPickerRow({title:'40代半ば',custom_item:'9'});
+	ageData[9] = Ti.UI.createPickerRow({title:'40代後半',custom_item:'10'});
+	ageData[10] = Ti.UI.createPickerRow({title:'50代以上',custom_item:'11'});
+
+	agePicker.add(ageData);
 
 	//ボタンをタップしたときの挙動
-	picker.addEventListener('change', function(e){
-		textField.value = e.row.title;
-	});
-
-	cancelButton.addEventListener('click', function(e){
-		textField.value = "";
-		pickerSlideOut(self, pickerView);
+	agePicker.addEventListener('change', function(e){
+		ageTextField.value = e.row.title;
 	});
 	
-	doneButton.addEventListener('click', function(e){
-		pickerSlideOut(self, pickerView);
+	ageDoneButton.addEventListener('click', function(e){
+		pickerSlideOut(self, agePickerView);
 	});
 
-	//createSearchWindow();
-
-	textField.addEventListener('click', function(){
-		Ti.API.info("clicked");
-		self.add(pickerView);
-		pickerSlideIn(pickerView);
+	ageTextField.addEventListener('click', function(){		
+		pickerSlideIn(self, agePickerView);
 	});
 	
+	//=========================================
+	//area系要素の定義
+	//=========================================
+	var areaTextField = Titanium.UI.createTextField({
+		borderStyle: Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
+		top: 80,
+		right: 50,
+		left: 50,
+		enabled: false,
+		keyboardToolbar: false
+	});
+	
+	//areaピッカービューのインスタンス生成
+	var areaPView = require('pickerView');
+	var areaPickerView = new areaPView();
+	
+	var areaChildren = areaPickerView.getChildren();
+	var areaToolbar = areaChildren[0];
+	var areaPicker = areaChildren[1];
+	
+	var areaItems = areaToolbar.getItems();
+	var areaDoneButton = areaItems[1];
+
+	//ピッカーにageデータを挿入
+	var areaData = [];
+	areaData[0] = Ti.UI.createPickerRow({title:'北海道',custom_item:'1'});
+	areaData[1] = Ti.UI.createPickerRow({title:'青森県',custom_item:'2'});
+	areaData[2] = Ti.UI.createPickerRow({title:'岩手県',custom_item:'3'});
+	areaData[3] = Ti.UI.createPickerRow({title:'宮城県',custom_item:'4'});
+	areaData[4] = Ti.UI.createPickerRow({title:'秋田県',custom_item:'5'});
+	areaData[5] = Ti.UI.createPickerRow({title:'山形県',custom_item:'6'});
+	areaData[6] = Ti.UI.createPickerRow({title:'福島県',custom_item:'7'});
+	areaData[7] = Ti.UI.createPickerRow({title:'茨城県',custom_item:'8'});
+	areaData[8] = Ti.UI.createPickerRow({title:'栃木県',custom_item:'9'});
+	areaData[9] = Ti.UI.createPickerRow({title:'群馬県',custom_item:'10'});
+	areaData[10] = Ti.UI.createPickerRow({title:'埼玉県',custom_item:'11'});
+	
+	areaPicker.add(areaData);
+	
+	//ボタンをタップしたときの挙動
+	areaPicker.addEventListener('change', function(e){
+		areaTextField.value = e.row.title;
+	});
+	
+	areaDoneButton.addEventListener('click', function(e){
+		pickerSlideOut(self, areaPickerView);
+	});
+
+	areaTextField.addEventListener('click', function(){		
+		pickerSlideIn(self, areaPickerView);
+	});
+
+
+
+	self.add(ageTextField);
+	self.add(areaTextField);
+
 	return self;
 }
 
-function pickerSlideIn(view) {
+module.exports = searchWindow;
+
+//=========================================
+//ファンクションの定義
+//=========================================
+function pickerSlideIn(win, view) {
 	var view = view;
+	var win = win;
+	win.add(view);
 	
 	view.animate({
 		bottom: 0,
@@ -90,6 +144,4 @@ function pickerSlideOut(win, view){
 		win.remove(view);
 	});
 }
-
-module.exports = searchWindow;
 
