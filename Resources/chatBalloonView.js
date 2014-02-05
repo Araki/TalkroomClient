8d5054ddbd440d93679618c5ba2afb4a1c9fe7e8
araki-k <araki-k@dhcp-10-11-70-038.35a.klab.org>
2014-02-05 22:26:49 +0900

    chatBalloonView.jsとchatWindow.jsがほぼ完成

--- Resources/chatBalloonView.js
+++ Resources/chatBalloonView.js
@@ -1,73 +1,136 @@
 //whichSide = "left" or "right"
 //message = メッセージ内容
 //imageURL = イメージのパス
+//chatViewHeight = このビューを設置する高さの位置
 
-function chatBalloonView(whichSide, whatMessage, whatImageURL ) {
+function chatBalloonView(whichSide, whatMessage, whatImageURL, chatViewHeight ) {
 	
 	var side = whichSide;
 	var message = whatMessage;
 	var imageURL = whatImageURL;
+	var viewHeight = chatViewHeight;
+	
+	var labelHeight;//テキストラベルの高さ
+	var limitLabelWidth = 200;//テキストラベルの横幅制限。このサイズを超える場合は複数行の処理。
+	
+	//Jsonのつなぎ込みができたら時刻はそのまま表示するが、現在時刻を表示するで代用
+	function getTime(){
+		// 日付の取得
+    	var date = new Date();
+    	//var year = date.getYear();
+    	var mon = date.getMonth() + 1;
+    	var day = date.getDate();
+
+    	// 西暦の処理とゼロパディング
+    	//year = (year < 2000) ? year+1900 : year;
+    	if (mon < 10) { mon = "0" + mon; }
+    	if (day < 10) { day = "0" + day; }
+
+    	// 時間の取得
+    	var time = new Date();
+    	var hour = time.getHours();
+    	var min = time.getMinutes();
+    	var sec = time.getSeconds();
+
+    	// ゼロパディング
+    	if (hour < 10) { hour = "0" + hour; }
+    	if (min < 10) { min = "0" + min; }
+    	if (sec < 10) { sec = "0" + sec; }
+
+    	// 書き出し
+    	NowDate = mon + "/" + day + " " + hour + ":" + min + ":" + sec;
+    	
+    	return NowDate;
+	}
 	
 	var self = Titanium.UI.createView({
-		height: 60,
-		bottom: 0
+		bottom: chatViewHeight,
+		//backgroundColor: "blue"
+	});
+	
+	var labelNameTime = Titanium.UI.createLabel({
+		text: getTime(),
+		font:{fontSize:10},
+		color:'#000'
 	});
 	
 	var iconImage = Titanium.UI.createImageView({
-	    	image: imageURL,//'http://profile.ak.fbcdn.net/hprofile-ak-prn2/276018_721214203_1913647351_q.jpg',
-	    	top: 0,
-	    	//left: 5,
-	    	width: 50,
-	    	height: 50
+	    	image: imageURL,
+	    	bottom: 10,
+	    	width: 30,
+	    	height: 30
 	});
 	
 	var balloonImage = Ti.UI.createView({
-    	//backgroundImage: 'WhiteBalloonLeft.png',
     	backgroundLeftCap: 24,
     	backgroundRightCap: 17,
     	backgroundTopCap: 14,
     	backgroundBottomCap: 17,
-    	height: 30,
-    	top: 0,
-    	//left: 60,
-    	//width: 100
+    	bottom: 10,
 	});
 	
 	var messageLabel = Ti.UI.createLabel({
-		//font:{fontSize:15}, 
-        //textAlign:'left',
+		font:{fontSize:14},
+		//backgroundColor: "white",
+		textAlign: "left",
+		verticalAlign: Titanium.UI.TEXT_VERTICAL_ALIGNMENT_CENTER,
         text: message,
        	color:'#000',
-       	top:3, 
-       	//right:30, 
-       	//width:170, 
-        //height:30	
+       	height: "auto",
+       	top:10,
 	});
 	
-	Ti.API.info("HEIGHT:" + messageLabel.toImage().height);
 	Ti.API.info("WIDTH:" + messageLabel.toImage().width);
+	Ti.API.info("HEIGHT:" + messageLabel.toImage().height);
 	
+	//テキストラベルの横幅がリミット値を超えた場合、ラベルを複数行にする処理
+	if (messageLabel.toImage().width > limitLabelWidth){
+		if (messageLabel.toImage().width % limitLabelWidth != 0){
+			labelHeight = (((messageLabel.toImage().width - ( messageLabel.toImage().width % limitLabelWidth )) / limitLabelWidth) + 1 ) * (messageLabel.font.fontSize + 4);
+		}else{
+			labelHeight = (messageLabel.toImage().width / limitLabelWidth) * (messageLabel.font.fontSize + 4);
+		}
+		//Ti.API.info("Label:" + labelHeight);
+		//Ti.API.info("FontSize:" + messageLabel.font.fontSize);
+		//messageLabelの縦、横サイズを指定
+		messageLabel.width = limitLabelWidth + 10;
+		messageLabel.height = labelHeight;
+		//balloonImageの縦、横サイズを指定
+		balloonImage.height = labelHeight + 20;
+		balloonImage.width = limitLabelWidth + 30;
+	}
+	//テキストラベルの横幅がリミット値を超えなかった場合、ラベルを1行にする処理
+	else if (messageLabel.toImage().width < limitLabelWidth){
+		labelHeight = messageLabel.font.fontSize + 4;
+		messageLabel.width = messageLabel.toImage().width + 10;
+		messageLabel.height = labelHeight;
+		balloonImage.height = labelHeight + 20;
+		balloonImage.width = limitLabelWidth + 30;
+	}
+	
+	balloonImage.width = messageLabel.width + 30;
+	self.height = balloonImage.height + 30;
+	labelNameTime.bottom = balloonImage.height + 10;
+	
+	//右バルーンか左バルーンか
 	if(side == "left"){
 		iconImage.left = 10;
-		balloonImage.left = 70;
-		balloonImage.width = messageLabel.toImage().width + 30;
+		balloonImage.left = 50;
 		balloonImage.backgroundImage = 'WhiteBalloonLeft.png';
-		messageLabel.textAlign = "left";
-		messageLabel.width = messageLabel.toImage().width;
 		messageLabel.left = 20;
+		labelNameTime.left = 10;
 	}else if(side == "right"){
 		iconImage.right = 10;
-		balloonImage.right = 70;
-		balloonImage.width = messageLabel.toImage().width + 30;
+		balloonImage.right = 50;
 		balloonImage.backgroundImage = 'GreenBalloonRight.png';
-		messageLabel.textAlign = "right";
-		messageLabel.width = messageLabel.toImage().width;
 		messageLabel.right = 20;
+		labelNameTime.right = 10;
 	}
 
 	balloonImage.add(messageLabel);
 	self.add(iconImage);
 	self.add(balloonImage);
+	self.add(labelNameTime);
 	
 	return self;
 }
--- Resources/chatWindow.js
+++ Resources/chatWindow.js
@@ -1,14 +1,87 @@
 function chatWindow() {
-
+	
+	var chatViewHeight = 0;
+	
+	//テストデータ始まり
+	testJsonData = new Array();
+	testJsonData[0] = {
+		"side":"right", 
+		"message":"こんにちは",
+	 	"image":"http://profile.ak.fbcdn.net/hprofile-ak-prn2/276018_721214203_1913647351_q.jpg"
+	 	 };
+	testJsonData[1] = {
+		"side":"left", 
+		"message":"こんにちは。何されているんですか〜？今日はお肉食べたい！",
+	 	"image":"http://static4.wikia.nocookie.net/__cb20120615021732/spongebob/images/6/6e/50px-5143827.png"
+	 	 };
+	testJsonData[2] = {
+		"side":"right", 
+		"message":"マジカルバナナマジカルバナナマジカルバナナマジカルバナナマジカルバナナマジカルバナナマジカルバナナマジカルバナナマジカルバナナマジカルバナナマジカルバナナマジカルバナナ",
+	 	"image":"http://profile.ak.fbcdn.net/hprofile-ak-prn2/276018_721214203_1913647351_q.jpg"
+	 	 };
+	testJsonData[3] = {
+		"side":"right", 
+		"message":"あなたは何を言ってるんですか？",
+	 	"image":"http://profile.ak.fbcdn.net/hprofile-ak-prn2/276018_721214203_1913647351_q.jpg"
+	 	 };
+	testJsonData[4] = {
+		"side":"left", 
+		"message":"HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello",
+	 	"image":"http://static4.wikia.nocookie.net/__cb20120615021732/spongebob/images/6/6e/50px-5143827.png"
+	 	 };
+	testJsonData[5] = {
+		"side":"left", 
+		"message":"abcdefghijklmnopqrstuvvvvvvvvv",
+	 	"image":"http://static4.wikia.nocookie.net/__cb20120615021732/spongebob/images/6/6e/50px-5143827.png"
+	 	 };
+	testJsonData[6] = {
+		"side":"left", 
+		"message":"abcdefghijklmnopqrstuvvvvvvvvv",
+	 	"image":"http://static4.wikia.nocookie.net/__cb20120615021732/spongebob/images/6/6e/50px-5143827.png"
+	 	 };
+	testJsonData[7] = {
+		"side":"left", 
+		"message":"abcdefghijklmnopqrstuvvvvvvvvv",
+	 	"image":"http://static4.wikia.nocookie.net/__cb20120615021732/spongebob/images/6/6e/50px-5143827.png"
+	 	 };
+	//テストデータ終わり
+	
 	var self = Titanium.UI.createWindow({  
 	    title:'チャット',
 	    backgroundColor:'#fff'
 	});
 	
-	var cbView = require('chatBalloonView');
-	var chatView = new cbView("right", "こんにちはこんにちは", "http://profile.ak.fbcdn.net/hprofile-ak-prn2/276018_721214203_1913647351_q.jpg");
-	self.add(chatView);
+	var scrollView = Titanium.UI.createScrollView({
+		contentWidth: "auto",
+		contentHeight: "auto",
+		top: 0,
+		bottom: 0,
+		showVerticalScrollIndicator: true
+	});
 	
+	for(var i=0; i<testJsonData.length; i++){
+		var cbView = require('chatBalloonView');
+		var chatView = new cbView(testJsonData[i]["side"], testJsonData[i]["message"], testJsonData[i]["image"], chatViewHeight);
+		scrollView.add(chatView);
+		chatViewHeight = chatViewHeight + chatView.height;
+	}
+	
+	Ti.API.info("scrollViewHeight:" + scrollView.toImage().height);
+	Ti.API.info("chatViewHeight:" + chatViewHeight);
+	
+	/*
+	function scrollview_scroll_handler(e){
+    	// e.y contains the current top y position of the contents of the scrollview
+    	Ti.API.info('Scrollview contents y offset: '+e.y);
+	}
+	scrollView.addEventListener('scroll', scrollview_scroll_handler);
+	*/
+	
+	scrollView.setContentOffset({x:0, y:scrollView.toImage().height - 455}, {animated:true});
+	//scrollView.contentOffset.y = scrollView.toImage().height - 455;
+	
+	self.add(scrollView);
+
 	return self;
 }
 
